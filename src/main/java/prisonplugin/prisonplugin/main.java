86f23a5507d53a1e484d4cb42a0fc01bb4144d4b
5vl <nederlandmax@gmail.com>
2021-01-16 22:44:28 +0100

    1.0.0

--- src/main/java/prisonplugin/prisonplugin/main.java
+++ src/main/java/prisonplugin/prisonplugin/main.java
@@ -1,17 +1,29 @@
 package prisonplugin.prisonplugin;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import prisonplugin.prisonplugin.utils.MetricsLite;
 
 public final class main extends JavaPlugin {
-
+    private static main instance;
+    String version = this.getDescription().getVersion();
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        System.out.println("Enabled Prison Plugin " + version);
+        instance = this;
+        this.getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        PluginManager plm = org.bukkit.Bukkit.getPluginManager();
+        int pluginId = 10028;
+        MetricsLite metrics = new MetricsLite(this, pluginId);
+        registerCMD();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public static main getInstance() {
+        return instance;
+    }
+
+    public void registerCMD() {
+
     }
 }
---
+++ src/main/java/prisonplugin/prisonplugin/utils/chatcolors.java
@@ -0,0 +1,5 @@
+package prisonplugin.prisonplugin.utils;
+import org.bukkit.ChatColor;
+public class chatcolors {
+    public String color(String string){return ChatColor.translateAlternateColorCodes('&', string);}
+}
